e1e1cf2c3ca534ec9b39e038e67a51c1de67d086
natia <nati.shonia37@gmail.com>
2022-04-27 13:59:09 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
+//join method ამატებს იმ ნიშანს რასაც ჩავწერთ ჯოინში
+
+
 let elements = ['georgia', 'usa', '123'];
 
 console.log(elements.join('+'));
+
+
+//shift პირველ ელემენტს შლის
+let element=['usa', 'jeolabi', 'georgia']
+element.shift();
+
+
+// unshift ამატებს ელემენტებს თავში
+let fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.unshift("Lemon", "Pineapple");
+
+
+//concat აერთიანებს არეის
+let arr1 = ["ჯეოლაბი", "123"];
+let arr2 = ["Emil", "phone", "computer"];
+let gaertianeba=arr1.concat(arr2); 
